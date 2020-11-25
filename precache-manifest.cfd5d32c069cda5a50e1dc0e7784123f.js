4ab94d047ee027448f87be65709db6eeb80753fb
lukocius <lukociusbest@gmail.com>
2020-11-25 12:08:37 +0200

    initial commit

---
+++ precache-manifest.cfd5d32c069cda5a50e1dc0e7784123f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8aef3ee651d34b098de359968171b74",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a299c283513198f2bb1c",
+    "url": "/static/css/main.7bcd52d0.chunk.css"
+  },
+  {
+    "revision": "259e61ea1c387fb917f5",
+    "url": "/static/js/2.8f86ec09.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.8f86ec09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a299c283513198f2bb1c",
+    "url": "/static/js/main.519a5749.chunk.js"
+  },
+  {
+    "revision": "8603aca29d046e40d9ff",
+    "url": "/static/js/runtime-main.9c121763.js"
+  }
+]);
